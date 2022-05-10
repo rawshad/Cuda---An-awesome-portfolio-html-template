6f3de9bcd9e268b86c04ae43f01805f3ea3f5406
rawshad <rawshad@outlook.com>
2022-05-10 17:00:36 +0600

    sticky navbar added

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,20 @@
 
-var containerEl = document.querySelector('.container');
+$(document).ready(function(){
+    $('.js--services-section').waypoint(function(direction){
+        if(direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }) 
+    var mixer = mixitup('.container');
+});
 
-            var mixer = mixitup(containerEl);
+// var containerEl = document.querySelector('.container');
+
+//             var mixer = mixitup(containerEl);
+
+
+
+ 
+  
